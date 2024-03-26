00a28ac448d2c69fa99e097c990dc911b8804624
chime-24x7-support <153920188+chime-24x7-support@users.noreply.github.com>
2024-03-26 21:32:41 +0000

    a

---
+++ tarp_0ffd14cde484068c61a03c41518f2348/index.php
@@ -0,0 +1,20 @@
+<?php
+$GLOBALS['_ta_rp_key'] = '00191b8136393f7a6542279af29be6fa';
+$GLOBALS['_ta_reverse_proxy_id'] = 'e84cf7';
+
+require 'bootloader_0ffd14cde484068c61a03c41518f2348.php';
+
+$options = array();
+/*
+$options = array(
+	'replace' => array(
+		'This is the text to find' => 'The new text',
+		'This is the text to find 2' => 'The new text 2',
+	)
+);
+*/
+
+$tarp = new TARPLoader($options);
+
+$tarp->excute();
+?>
